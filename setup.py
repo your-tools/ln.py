c2d47c2e19bec13c1b0e87b08aeb9e30332cbc43
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-04-02 17:20:37 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name="ln.py",
-      version="0.2",
+      version="0.3",
       description="symlinks made easier",
       url="https://github.com/dmerejkowsky/ln.py",
       author="Dimitri Merejkowsky",
       author_email="d.merej@gmail.com",
+      py_modules=["ln"],
       entry_points={
         "console_scripts": [
           "lnp = ln:main",
